229163ae62639dd0d7b91808b9d2290c817352ab
JonasP <jp.developer90@gmail.com>
2022-03-30 14:07:28 -0300

    Se crea html y .js

---
+++ main.js
@@ -0,0 +1,12 @@
+let nombre = prompt("Ingrese su nombre: ");
+let apellido = prompt("Ingrese sus apellido: ");
+let edad = prompt("Ingres su edad : ");
+let saludo = "Hola" + " " + nombre;
+
+let numero1 = prompt("Ingrese un numero : ");
+let numero2 = prompt("Ingrese otro numero : ");
+let suma = parseInt(numero1) + parseInt(numero2);
+
+let calculo = "La suma de sus numero fue:" + suma;
+alert(saludo);
+alert(calculo);
